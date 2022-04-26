b38bbfdb6b376c7aa85a3048f3bb0e3ca7ca0204
r.ziafaty <r.ziafaty@hotmail.com>
2022-04-26 03:53:58 -0400

    graceful Shutdown by signal or a handler close#6

--- listen.go
+++ listen.go
@@ -1,15 +1,59 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 	"windows/config"
 
 	"github.com/gorilla/mux"
 )
 
+type serverStruct struct {
+	httpServer  *http.Server
+	shutdownReq chan bool
+	reqCount    uint32
+}
+
+func createNewServer(httpServer *http.Server) *serverStruct {
+	//create server
+	return &serverStruct{
+		httpServer:  httpServer,
+		shutdownReq: make(chan bool),
+	}
+}
+
+func (server *serverStruct) WaitShutdown(stopLog string) {
+	irqSig := make(chan os.Signal, 1)
+	signal.Notify(irqSig, syscall.SIGINT, syscall.SIGTERM)
+
+	//Wait interrupt or shutdown request through /shutdown
+	select {
+	case sig := <-irqSig:
+		log.Printf("Shutdown request (signal: %v)", sig)
+	case sig := <-server.shutdownReq:
+		log.Printf("Shutdown request (/shutdown %v)", sig)
+	}
+
+	log.Println(stopLog)
+
+	//Create shutdown context with 10 second timeout
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	//shutdown the server
+	err := server.httpServer.Shutdown(ctx)
+	if err != nil {
+		log.Printf("Shutdown request error: %v", err)
+	}
+}
+
 func listenHTTP(router *mux.Router) {
+
 	httpServer := &http.Server{
 		Addr:           config.MainConfig.WebServerS.HttpAddr,
 		Handler:        router,
@@ -17,8 +61,23 @@ func listenHTTP(router *mux.Router) {
 		WriteTimeout:   30 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
-	log.Println("HTTP server listen:", httpServer.Addr)
-	log.Fatal(httpServer.ListenAndServe())
+	server := createNewServer(httpServer)
+
+	done := make(chan bool)
+	go func() {
+		log.Println("HTTP server listen:", httpServer.Addr)
+		err := server.httpServer.ListenAndServe()
+		if err != nil {
+			log.Printf("Listen and serve: %v", err)
+		}
+		done <- true
+	}()
+
+	//wait shutdown
+	server.WaitShutdown("HTTP Server shutting down Addr: " + httpServer.Addr)
+
+	<-done
+	log.Printf("HTTP server shutdown completed Addr: " + httpServer.Addr)
 
 }
 
@@ -43,7 +102,21 @@ func listenHTTPS(router *mux.Router) {
 		WriteTimeout:   30 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
+	server := createNewServer(httpServer)
 
-	log.Println("HTTPS server listen:", httpServer.Addr)
-	log.Fatal(httpServer.ListenAndServeTLS(config.MainConfig.WebServerS.Cert, config.MainConfig.WebServerS.Key))
+	done := make(chan bool)
+	go func() {
+		log.Println("HTTPS server listen:", httpServer.Addr)
+		err := server.httpServer.ListenAndServeTLS(config.MainConfig.WebServerS.Cert, config.MainConfig.WebServerS.Key)
+		if err != nil {
+			log.Printf("Listen and serve TLS: %v", err)
+		}
+		done <- true
+	}()
+
+	//wait shutdown
+	server.WaitShutdown("HTTPS Server shutting down Addr: " + httpServer.Addr)
+
+	<-done
+	log.Printf("HTTPS server shutdown completed Addr: " + httpServer.Addr)
 }
