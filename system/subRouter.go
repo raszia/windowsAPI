6c463e9ffdb29ab5c50bb4969f900268eef9c63a
r.ziafaty <r.ziafaty@hotmail.com>
2022-04-30 04:42:06 -0400

    interface,mem,disk info added

--- system/subRouter.go
+++ system/subRouter.go
@@ -9,10 +9,42 @@ import (
 
 func SubRoute(router *mux.Router) {
 	router.HandleFunc("/info", infoHandler).Methods(http.MethodGet)
-	// router.HandleFunc("/vmem", memHandler).Methods(http.MethodGet)
+	router.HandleFunc("/vmem", memHandler).Methods(http.MethodGet)
 	router.HandleFunc("/process", processHandler).Methods(http.MethodGet)
-	// router.HandleFunc("/info", getInfoHandler).Methods(http.MethodGet)
-	// router.HandleFunc("/info", getInfoHandler).Methods(http.MethodGet)
+	router.HandleFunc("/interface", interfaceHandler).Methods(http.MethodGet)
+	router.HandleFunc("/disk/{operation}", diskHandler).Methods(http.MethodGet)
+}
+
+func diskHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	switch vars["operation"] {
+	case "usage":
+	case "partitions":
+	case "iocounters":
+	}
+	iStat, err := GetInterfaces(r.Context())
+	if err != nil {
+		utility.HttpConnectionClose(w, r, http.StatusNotAcceptable, &utility.ResStruct{
+			Msg:    err.Error(),
+			Status: "failed",
+		})
+		return
+	}
+	utility.HttpSendOK(w, r, iStat)
+}
+
+func interfaceHandler(w http.ResponseWriter, r *http.Request) {
+
+	iStat, err := GetInterfaces(r.Context())
+	if err != nil {
+		utility.HttpConnectionClose(w, r, http.StatusNotAcceptable, &utility.ResStruct{
+			Msg:    err.Error(),
+			Status: "failed",
+		})
+		return
+	}
+	utility.HttpSendOK(w, r, iStat)
 }
 
 func memHandler(w http.ResponseWriter, r *http.Request) {
--- system/systemInfo.go
+++ system/systemInfo.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/shirou/gopsutil/mem"
+	"github.com/shirou/gopsutil/net"
 	"github.com/shirou/gopsutil/v3/host"
 )
 
@@ -15,10 +16,26 @@ func GetInfo(ctx context.Context) (*host.InfoStat, error) {
 	return info, nil
 }
 
-func Getvmem(ctx context.Context) (*mem.VirtualMemoryExStat, error) {
-	vmem, err := mem.VirtualMemoryExWithContext(ctx)
+func Getvmem(ctx context.Context) (*mem.VirtualMemoryStat, error) {
+	vmem, err := mem.VirtualMemoryWithContext(ctx)
 	if err != nil {
 		return nil, err
 	}
 	return vmem, nil
+}
+
+func GetInterfaces(ctx context.Context) ([]net.InterfaceStat, error) {
+	iStat, err := net.InterfacesWithContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return iStat, nil
+}
+
+func GetDisk(ctx context.Context) ([]net.InterfaceStat, error) {
+	iStat, err := net.InterfacesWithContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return iStat, nil
 }
